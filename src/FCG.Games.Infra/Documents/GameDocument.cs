b96482f4b353e3de3b39cedc8ac07dfcb72eee2b
João Melo <devjoaomelo@gmail.com>
2025-10-06 23:28:15 -0300

    Infra: DTO para index

---
+++ src/FCG.Games.Infra/Documents/GameDocument.cs
@@ -0,0 +1,20 @@
+﻿using FCG.Games.Domain.Entities;
+
+namespace FCG.Games.Infra.Documents;
+
+// DTO para indexação no OpenSearch
+public sealed class GameDocument
+{
+    public Guid Id { get; init; }
+    public string Title { get; init; } = default!;
+    public string Description { get; init; } = default!;
+    public decimal Price { get; init; }
+
+    public static GameDocument FromDomain(Game g) => new()
+    {
+        Id = g.Id,
+        Title = g.Title.Value,
+        Description = g.Description.Value,
+        Price = g.Price.Value
+    };
+}
