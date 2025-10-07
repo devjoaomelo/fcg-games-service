e648a5ea535a449b514b91187f908ff9b5d00ee6
João Melo <devjoaomelo@gmail.com>
2025-10-06 23:29:15 -0300

    Domain: Game Creation Service

---
+++ src/FCG.Games.Domain/Services/GameCreationService.cs
@@ -0,0 +1,17 @@
+﻿using FCG.Games.Domain.Entities;
+using FCG.Games.Domain.Interfaces;
+using FCG.Games.Domain.ValueObjects;
+
+namespace FCG.Games.Domain.Services;
+
+public sealed class GameCreationService : IGameCreationService
+{
+    public Game Create(string title, string? description, decimal price)
+    {
+        var t = GameTitle.Create(title);
+        var d = Description.Create(description);
+        var p = Price.Parse(price);
+
+        return new Game(t, d, p);
+    }
+}
